704b7301fa191de5579a37686f67a4e217021e0b
Aytuhan Kazandirmak <aytuhann@gmail.com>
2023-11-07 08:35:33 +0300

    general update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Footer() {
-  return <div className="footer">Made with by Aytuhan Kaznadırmak</div>;
+  return (
+    <div className="footer">
+      Made with by{" "}
+      <a
+        className="footer-link"
+        href="https://www.linkedin.com/in/aytuhan-kazandırmak-8003a1226/"
+      >
+        Aytuhan Kazandırmak
+      </a>
+    </div>
+  );
 }
 
 export default Footer;
